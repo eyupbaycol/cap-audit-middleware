523fec94acabb361e52c7a19912ef3d09725a881
eyupbaycol <eyup.bycl@gmail.com>
2025-03-28 01:52:22 +0300

    delete usage simple

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const AuditMiddleware = require('./lib/audit-middleware');
 const DatabaseStorage = require('./lib/storage/database');
 const JsonFileStorage = require('./lib/storage/json-file');
 const WebhookStorage = require('./lib/storage/webhook');
-const { AuditLogService } = require('./lib/ui/audit-service');
-const { setupAuditUI, setupAuditService } = require('./lib/ui/setup');
 
 module.exports = {
   AuditMiddleware,
@@ -12,9 +10,4 @@ module.exports = {
     JsonFileStorage,
     WebhookStorage
   },
-  ui: {
-    AuditLogService,
-    setupAuditUI,
-    setupAuditService
-  }
 }; 
